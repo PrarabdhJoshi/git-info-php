f9b05f93b005c9bf8c1cb584b7f8d5ffa01a74c6
PrarabdhJoshi <prarabdh@ufl.edu>
2018-02-01 11:40:41 -0500

    consumed GET

--- git-info.php
+++ git-info.php
@@ -1,7 +1,8 @@
 <?php
 
-require_once 'vendor/autoload.php';
 
+
+require_once 'vendor/autoload.php';
 $cmd = new Commando\Command();
 
 // Define first option
@@ -9,4 +10,19 @@ $cmd->option()
     ->require()
     ->describedAs('GitHub Username');
 
+echo "Public Repositories for {$cmd[0]}:", PHP_EOL;
 
+
+$opts = array(
+    'http'=>array(
+      'method'=>"GET",
+      'header'=>"User-agent: PrarabdhJoshi\r\n"
+    )
+  );
+  
+$context = stream_context_create($opts);
+  
+  // Open the file using the HTTP headers set above
+$json = file_get_contents("https://api.github.com/users/{$cmd[0]}/repos", false, $context);
+$obj = json_decode($json);
+echo $obj[0]->id,PHP_EOL;
