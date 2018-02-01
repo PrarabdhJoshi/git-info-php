17593c9e1dd96803b89188f2065a8a76281b3050
PrarabdhJoshi <prarabdh@ufl.edu>
2018-02-01 12:47:55 -0500

    ordering implemented

--- git-info.php
+++ git-info.php
@@ -1,16 +1,18 @@
 <?php
 
-
-
 require_once 'vendor/autoload.php';
-$cmd = new Commando\Command();
+$cmd = new Commando\Command(); //instantiate a new command
 
 // Define first option
 $cmd->option()
     ->require()
     ->describedAs('GitHub Username');
 
-echo "Public Repositories for {$cmd[0]}:", PHP_EOL;
+
+//get the argument for ascending or descending values
+$cmd->option('o')
+->aka('order')
+->describedAs('type asc to display a sorted result or dsc to display a reversed sorted result based on Stargazers_count');
 
 
 $opts = array(
@@ -29,10 +31,16 @@ $obj = json_decode($json);
 //sort the array function
 function cmp($a, $b)
 {
+    return strcmp($a->stargazers_count,$b->stargazers_count);
+}
+//reverse sort the array
+function rcmp($a, $b)
+{
     return strcmp($b->stargazers_count,$a->stargazers_count);
 }
 //call the sorting on the object
-usort($obj, "cmp");
+($cmd[1]=="dsc")?usort($obj, "rcmp"):usort($obj,"cmp");
+
 
 for ($x = 0; $x < count((array)$obj); $x++) {
     echo "{$obj[$x]->name} \t {$obj[$x]->stargazers_count}",PHP_EOL;
